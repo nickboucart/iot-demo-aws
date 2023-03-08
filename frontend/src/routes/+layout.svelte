e6d2cc1e2650638b036860564cf2b2963d17ae41
Nick Boucart <nboucart@gmail.com>
2023-03-08 09:42:57 +0100

    more styling

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Footer from "$lib/components/Footer.svelte";
+import "../app.css";
+</script>
+
+<slot />
+<Footer />
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,8 +12,9 @@ const config = {
 			precompress: false,
 			pages: 'dist'
 		}),
-		prerender: {entries: []}
-	}
+		prerender: {entries: []},
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
